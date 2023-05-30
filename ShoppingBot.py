3d35c1403ff2a9f9d1617bcc025fec9dc5a9e70a
Eran <eran.levi3@mail.huji.ac.il>
2023-05-30 18:27:28 +0300

    format

--- ShoppingBot.py
+++ ShoppingBot.py
@@ -1,14 +1,10 @@
-
-"""
-
-"""
 import os
 import json
 import telebot
 from telebot import types
 
 
-API_TOKEN = os.environ['API_TOKEN_E']
+API_TOKEN = os.environ["API_TOKEN_E"]
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -26,11 +22,12 @@ class User:
         self.current_divisions_list = {}
 
 
-
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=["help", "start"])
 def send_welcome(message):
-    msg = bot.reply_to(message, """\
+    msg = bot.reply_to(
+        message,
+        """\
 שלום!, אני רובוט רשימת הקניות שלך.
 בכל עת תוכל לשלוח לי את רשימת הקניות שלך (כל מוצר בשורה חדשה) ואני אסדר לפי מחלקות :)\
 \n
@@ -38,7 +35,8 @@ def send_welcome(message):
 שליחת add/ תאפשר הוספת מוצרים חדשים למחלקה קיימת\n
 
 במידה וחסרה מחלקה אנא פנה ליוצר שלי
-""")
+""",
+    )
     # print(msg.text)
     # -> gives Hi there, I am Example bot. What's your name?
     bot.register_next_step_handler(msg, process_name_step)
@@ -61,8 +59,9 @@ def process_name_step(message):
             if div not in user.current_divisions_list.keys():
                 user.current_divisions_list[div] = item
             else:
-                user.current_divisions_list[div] = user.current_divisions_list[
-                                                  div] + "\n" + item
+                user.current_divisions_list[div] = (
+                    user.current_divisions_list[div] + "\n" + item
+                )
             at_least_1_item_found = True
         else:
             print("הפריט " + item + " איננו מוכר לי. ")
@@ -74,10 +73,12 @@ def process_name_step(message):
     # except Exception as e:
     #     bot.reply_to(message, 'oooops')
 
+
 def strip_start_and_end(shopping_list):
     for i in range(len(shopping_list)):
         shopping_list[i] = (shopping_list[i].lstrip()).rstrip()
     return shopping_list
+
 
 def pretty_output(user):
     ordered_output = ""
@@ -86,7 +87,8 @@ def pretty_output(user):
         ordered_output += user.current_divisions_list[div] + "\n\n"
     return ordered_output
 
-def find_item(json_data, item,user):
+
+def find_item(json_data, item, user):
     found, div = (False, None)
     for div in user.json_data["מחלקות"]:
         for product in user.json_data["מחלקות"][div]:
@@ -95,12 +97,15 @@ def find_item(json_data, item,user):
                 return found, div
     return found, div
 
+
 # Handle '/add'
-@bot.message_handler(commands=['add'])
+@bot.message_handler(commands=["add"])
 def send_add_welcome(message):
     chat_id = message.chat.id
     user = User(chat_id)
-    msg = bot.reply_to(message, """\
+    msg = bot.reply_to(
+        message,
+        """\
 :אנא הכנס בשורה הראשונה את שם המחלקה המתאימה שברצונך להוסיף אליה מוצרים (מבין המחלקות הבאות) 
 ובשורות הבאות פריט בשורה ולאחריו מחיר הפריט לדוגמה:\n
 מוצרי חלב וביצים
@@ -110,10 +115,12 @@ def send_add_welcome(message):
 5
 
 לבדיקת שייכות מוצר למחלקה: https://www.shukcity.co.il/categories?level1=79653
-""")
+""",
+    )
     for i in range(len(user.divisions_list)):
-        bot.send_message(chat_id, str(user.divisions_list[i])+'\n')
+        bot.send_message(chat_id, str(user.divisions_list[i]) + "\n")
     bot.register_next_step_handler(msg, process_add)
+
 
 def process_add(message):
     # try:
@@ -125,32 +132,42 @@ def process_add(message):
     lines = strip_start_and_end(lines)
     div = lines[0]
     if div not in user.json_data["מחלקות"]:
-        bot.send_message(chat_id, 'נראה שהמחלקה שהזנת לא מתאימה למחלקות הקיימות עליך לנסות שוב מהתחלה על ידי שליחת add\ ')
+        bot.send_message(
+            chat_id,
+            "נראה שהמחלקה שהזנת לא מתאימה למחלקות הקיימות עליך לנסות שוב מהתחלה על ידי שליחת add\ ",
+        )
         return
     for i in range(len(lines)):
-        if i==0:
+        if i == 0:
             continue
         else:
-            if i%2==1:
+            if i % 2 == 1:
                 item = lines[i]
-                i=i+1
+                i = i + 1
                 price = lines[i]
                 found, some_div = find_item(user.json_data, item, user)
                 if found:
-                    bot.send_message(chat_id, 'הפריט '+item+' כבר מופיע במחלקה '+str(div))
+                    bot.send_message(
+                        chat_id, "הפריט " + item + " כבר מופיע במחלקה " + str(div)
+                    )
                 else:
                     user.json_data["מחלקות"][div].append(item)
                     # user.f = open("/home/Eran/pyTelegramBotAPI/examples/data_file.json", 'w')
-                    user.f = open("data_file.json", 'w')
+                    user.f = open("data_file.json", "w")
                     json.dump(user.json_data, user.f)
                     user.f.close()
-                    print("Someone added: "+item+" to "+str(div))
-                    bot.send_message(chat_id, 'הפריט '+item+ ' התווסף בהצלחה למחלקת '+str(div)+'. תודה ' )
+                    print("Someone added: " + item + " to " + str(div))
+                    bot.send_message(
+                        chat_id,
+                        "הפריט "
+                        + item
+                        + " התווסף בהצלחה למחלקת "
+                        + str(div)
+                        + ". תודה ",
+                    )
     # except Exception as e:
     #     bot.reply_to(message, 'oooops')
     return
-
-
 
 
 # Enable saving next step handlers to file "./.handlers-saves/step.save".
